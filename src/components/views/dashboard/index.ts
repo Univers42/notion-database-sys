8c7797b7964e5a5f969c1ded06c7142a4eb062f7
dlesieur <dlesieur@dev.local>
2026-03-31 16:26:00 +0200

    components: slim WidgetRenderer from 341 to 63 lines

--- src/components/views/dashboard/index.ts
+++ src/components/views/dashboard/index.ts
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   index.ts                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlesieur <dlesieur@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/01 16:37:33 by dlesieur          #+#    #+#             */
+/*   Updated: 2026/04/01 16:37:34 by dlesieur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 export { COLORS, STAT_COLORS, formatNumber, formatCellValue, smoothLine } from './constants';
 export { StatIconBadge, StatCard, EmptyWidget } from './StatComponents';
 export { BarChartWidget, NumberSummaryWidget, RecentList } from './DataComponents';
