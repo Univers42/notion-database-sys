5ff5729a9a98667671ebb519df18a79802fb954f
dlesieur <dlesieur@dev.local>
2026-03-31 14:18:00 +0200

    hooks: update barrel and clean up hook imports

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   index.ts                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlesieur <dlesieur@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/01 16:40:02 by dlesieur          #+#    #+#             */
+/*   Updated: 2026/04/01 16:40:03 by dlesieur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 export { useResizablePanel, MIN_WIDTH, MAX_WIDTH_RATIO } from './useResizablePanel';
 export { useBlockEditor } from './useBlockEditor';
