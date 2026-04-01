04d22e379ba3ad9897b5419ae34c3d8af883f1a9
dlesieur <dlesieur@dev.local>
2026-04-01 10:18:00 +0200

    components/views/table: update table module imports

--- src/components/views/table/index.ts
+++ src/components/views/table/index.ts
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   index.ts                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlesieur <dlesieur@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/01 16:37:47 by dlesieur          #+#    #+#             */
+/*   Updated: 2026/04/01 16:37:48 by dlesieur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 export { MemoTableRow } from './MemoTableRow';
 export type { MemoTableRowProps } from './MemoTableRow';
 export { SelectEditor, MultiSelectEditor } from './SelectEditors';
