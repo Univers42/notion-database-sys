7e8e324040f9735e1ff3f79142cc8afc6fdf077c
dlesieur <dlesieur@dev.local>
2026-04-01 15:50:24 +0200

    chore: update src/components/topBar/index.ts (#67 at 2026-04-01T15:50:24+0200)

---
+++ src/components/topBar/index.ts
@@ -0,0 +1,4 @@
+export { useOutsideClick, Dropdown } from './Dropdown';
+export { VIEW_ICONS, VIEW_TYPE_CARD_ICONS, VIEW_TYPE_ORDER, VIEW_LABELS } from './constants';
+export { SortPanel } from './SortPanel';
+export { ExtraActionsMenu, ViewDotsMenu, ActiveViewMenu } from './MenuComponents';
