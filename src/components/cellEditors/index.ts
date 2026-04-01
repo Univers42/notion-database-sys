6a40794d7ba2b837b96f5b55c8d5f088d9ce89c4
dlesieur <dlesieur@dev.local>
2026-04-01 12:00:00 +0200

    chore: update src/components/cellEditors/index.ts (#51 at 2026-04-01T12:00:00+0200)

---
+++ src/components/cellEditors/index.ts
@@ -0,0 +1,7 @@
+export { CellPortal, useCellRect } from './CellPortal';
+export { ROLLUP_FUNCTIONS, getDotColor } from './constants';
+export type { IdFormat } from './constants';
+export { RelationCellEditor } from './RelationCellEditor';
+export { RollupCellEditor } from './RollupCellEditor';
+export { StatusCellEditor } from './StatusCellEditor';
+export { IdCellEditor } from './IdCellEditor';
