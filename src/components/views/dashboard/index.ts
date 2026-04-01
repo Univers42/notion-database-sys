853c61fe5105987d848fc40dd21d333aaa3204e0
dlesieur <dlesieur@dev.local>
2026-04-01 18:43:12 +0200

    chore: update src/components/views/dashboard/index.ts (#79 at 2026-04-01T18:43:12+0200)

---
+++ src/components/views/dashboard/index.ts
@@ -0,0 +1,7 @@
+export { COLORS, STAT_COLORS, formatNumber, formatCellValue, smoothLine } from './constants';
+export { StatIconBadge, StatCard, EmptyWidget } from './StatComponents';
+export { BarChartWidget, NumberSummaryWidget, RecentList } from './DataComponents';
+export { DonutChart, AreaChartSVG, ProgressRing } from './SVGCharts';
+export { MultiLineChart } from './MultiLineChart';
+export { renderWidget } from './WidgetRenderer';
+export type { ComputedData } from './WidgetRenderer';
