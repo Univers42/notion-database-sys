44870b440a359a339c63b03446e5789e92431610
dlesieur <dlesieur@dev.local>
2026-04-01 17:02:24 +0200

    chore: update src/components/viewSettings/index.ts (#72 at 2026-04-01T17:02:24+0200)

---
+++ src/components/viewSettings/index.ts
@@ -0,0 +1,11 @@
+export { VIEW_META, CHART_TYPE_META, LAYOUT_ORDER, DEFAULT_VIEW_ICONS, DEFAULT_PROPERTY_ICONS } from './constants';
+export type { PanelScreen } from './constants';
+export { SubPanelHeader, OptionList, PropertyOptionList, Toggle, CardLayoutPicker, ViewIdentityRow, PropertyVisibilityRow } from './SubComponents';
+export { LayoutScreen } from './LayoutScreen';
+export type { LayoutScreenProps } from './LayoutScreen';
+export {
+  EditChartScreen, ChartTypeScreen, XAxisWhatScreen, XAxisSortScreen,
+  YAxisWhatScreen, YAxisGroupByScreen, YAxisRangeScreen, YAxisReferenceLineScreen,
+  ColorPaletteScreen, MoreStyleScreen,
+} from './ChartScreens';
+export type { ChartScreensProps } from './ChartScreens';
