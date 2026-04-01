c55bb5e8938700942d3e586d9067df3d43233033
dlesieur <dlesieur@dev.local>
2026-04-01 14:38:24 +0200

    chore: update src/components/formulaEditor/index.ts (#62 at 2026-04-01T14:38:24+0200)

---
+++ src/components/formulaEditor/index.ts
@@ -0,0 +1,6 @@
+export { FORMULA_FUNCTIONS, FUNCTION_CATEGORIES, getReturnTypeBadge, propReturnType } from './catalog';
+export type { FunctionDef } from './catalog';
+export { PropTypeIcon } from './PropTypeIcon';
+export { ExampleBlock } from './ExampleBlock';
+export { Sidebar } from './Sidebar';
+export { DocPanel } from './DocPanel';
